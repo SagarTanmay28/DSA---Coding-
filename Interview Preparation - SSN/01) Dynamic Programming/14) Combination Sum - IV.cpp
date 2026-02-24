d4d9b85468f01f9ecf50a7b3fbf66872f44b3246
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-24 19:36:54 +0530

    Create 14) Combination Sum - IV.cpp

---
+++ Interview Preparation - SSN/01) Dynamic Programming/14) Combination Sum - IV.cpp
@@ -0,0 +1,23 @@
+class Solution { // ***Follow Up***
+public:
+    int combinations(vector<int>& nums, int target, vector<int>& dp){
+        if(target == 0) return 1;
+        if(target < 0) return 0;
+
+        if(dp[target] != -1) return dp[target];
+
+        int ans = 0;
+        for(int i=0;i<nums.size();i++){
+            ans += combinations(nums,target - nums[i],dp);
+        }
+
+        return dp[target] = ans;
+    }
+    int combinationSum4(vector<int>& nums, int target) {
+        int n = nums.size();
+        vector<int> dp(target+1,-1);
+
+        return combinations(nums,target,dp);
+        
+    }
+};
