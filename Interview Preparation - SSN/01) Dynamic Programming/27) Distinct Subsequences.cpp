53034ed7b14d52f9585b084a8a372c76fc51e218
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-24 23:57:39 +0530

    Create 27) Distinct Subsequences.cpp

---
+++ Interview Preparation - SSN/01) Dynamic Programming/27) Distinct Subsequences.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int helper(string& s, string& t, int i, int j, vector<vector<int>>& dp){
+        if(j == t.size()) return 1;
+        if(i == s.size()) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(s[i] == t[j]){
+            return dp[i][j] = helper(s,t,i+1,j+1,dp) + helper(s,t,i+1,j,dp);
+        }
+        else{
+            return dp[i][j] = helper(s,t,i+1,j,dp);
+        }
+    }
+    int numDistinct(string s, string t) {
+        int n = s.size();
+        int m = t.size();
+
+        vector<vector<int>> dp(n,vector<int>(m,-1));
+
+        return helper(s,t,0,0,dp);
+        
+    }
+};
