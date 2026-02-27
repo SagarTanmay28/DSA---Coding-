d5cb964c211549233aad2ab4d6e36dc188244679
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-02-27 18:57:50 +0530

    Create 20) Max Sum Subarray of Size K.cpp

---
+++ Interview Preparation - SSN/02) Sliding Window/20) Max Sum Subarray of Size K.cpp
@@ -0,0 +1,28 @@
+class Solution {
+  public:
+    int maxSubarraySum(vector<int>& arr, int k) {
+        // code here
+        int n = arr.size();
+        int prevSum = 0;
+        
+        
+        for(int i=0;i<k;i++){
+            prevSum += arr[i];
+        }
+        
+        int maxSum = prevSum;
+        
+        int i = 1;
+        int j = k;
+        
+        while(j < n){
+            int currSum = prevSum + arr[j] - arr[i-1];
+            if(currSum > maxSum) maxSum = currSum;
+            prevSum = currSum;
+            i++;
+            j++;
+        }
+        return maxSum;
+        
+    }
+};
