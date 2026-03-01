5e3cae0b1d0cbd0c3654399bc3388e7460c477df
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-03-01 19:29:22 +0530

    Create 36) LCA of a BST.cpp

---
+++ Interview Preparation - SSN/03) Binary Trees and Binary Search Trees/36) LCA of a BST.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        if(root == NULL || root == p || root == q) return root;
+
+        TreeNode* left = lowestCommonAncestor(root->left,p,q);
+        TreeNode* right = lowestCommonAncestor(root->right,p,q);
+
+        if(left && right) return root;
+
+        return left ? left : right;
+    }
+};
+
+// BST Property
+class Solution { // 2nd Solution 
+public:
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+         if(root->val < p->val && root->val < q->val) return lowestCommonAncestor(root->right,p,q);
+         else if(root->val > p->val && root->val > q->val) return lowestCommonAncestor(root->left,p,q);
+         return root;
+    }
+};
