70a4d998d0e9f3790b9dbf4e024dd7196a0f3a63
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-03-03 00:03:40 +0530

    Create 31) Vertical Order Traversal of a Binary Tree.cpp

---
+++ Interview Preparation - SSN/03) Binary Trees and Binary Search Trees/31) Vertical Order Traversal of a Binary Tree.cpp
@@ -0,0 +1,48 @@
+class Solution {
+public:
+    vector<vector<int>> verticalTraversal(TreeNode* root) {
+
+        map<int, map<int, multiset<int>>> mp;
+
+        // queue 
+        queue<pair<TreeNode*,pair<int,int>>> q;
+        q.push({root,{0,0}});
+
+        while(q.size() > 0){
+
+            auto front = q.front();
+            q.pop();
+
+            TreeNode* temp = front.first;
+            int row = front.second.first;
+            int col = front.second.second;
+
+            mp[col][row].insert(temp->val);
+
+            if(temp->left != NULL){
+                q.push({temp->left,{row + 1,col - 1}});
+            }
+
+            if(temp->right != NULL){
+                q.push({temp->right,{row + 1,col + 1}});
+            }
+        }
+
+        vector<vector<int>> ans;
+
+        for(auto &x : mp){
+
+            vector<int> nums;
+
+            for(auto &y : x.second){
+                nums.insert(nums.end(),
+                            y.second.begin(),
+                            y.second.end());
+            }
+
+            ans.push_back(nums);
+        }
+        
+        return ans;
+    }
+};
