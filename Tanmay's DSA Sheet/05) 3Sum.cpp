e15beed1436095f94e250f69fcb4ef180f947dc6
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-11 22:47:34 +0530

    Create 05) 3Sum.cpp

---
+++ Tanmay's DSA Sheet/05) 3Sum.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+vector<vector<int>> threeSum(vector<int>& nums) {
+    vector<vector<int>> result;
+        int n = nums.size();
+        
+        // Sort the array
+        sort(nums.begin(), nums.end());
+        
+        for (int i = 0; i < n; ++i) {
+            // Skip duplicates for the first element
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            
+            int left = i + 1, right = n - 1;
+            while (left < right) {
+                int sum = nums[i] + nums[left] + nums[right];
+                
+                if (sum == 0) {
+                    result.push_back({nums[i], nums[left], nums[right]});
+                    
+                    // Skip duplicates for the second element
+                    while (left < right && nums[left] == nums[left + 1]) ++left;
+                    // Skip duplicates for the third element
+                    while (left < right && nums[right] == nums[right - 1]) --right;
+                    
+                    ++left;
+                    --right;
+                } else if (sum < 0) {
+                    ++left;
+                } else {
+                    --right;
+                }
+            }
+        }
+        
+        return result;
+    }
+};
