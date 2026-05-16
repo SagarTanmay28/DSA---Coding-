924ca5d697a6fc224d66fcea4c0e753e8a9e5e6e
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-16 15:35:37 +0530

    Update _03_Binary Exponentiation.cpp

--- Mathematics/_03_Binary Exponentiation.cpp
+++ Mathematics/_03_Binary Exponentiation.cpp
@@ -15,20 +15,26 @@ long long binExp(long long x, long long n) {
 
 ***********************************************************************************_Binary Exponentiation with Modulo (%)_*********************************************************************************************
 
-TC: O(log n)
-SC: O(1)
-🔥🔥 Used everywhere (MOD = 1e9+7)
+// TC: O(log n)
+// SC: O(1)
+// Binary Exponentiation / Fast Power
 
 const long long MOD = 1e9 + 7;
-long long modPow(long long x, long long n, long long MOD){
+
+long long modPow(long long x, long long n) {
     long long ans = 1;
-    while(n > 0){
-        if(n % 2 != 0){
+
+    x %= MOD;
+
+    while (n > 0) {
+        if (n % 2 == 1) {
             ans = (ans * x) % MOD;
         }
+
         x = (x * x) % MOD;
         n /= 2;
     }
+
     return ans;
 }
 
