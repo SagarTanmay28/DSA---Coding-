a51fab6f6e3a6f9ac3df847131905948a537dacc
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-20 00:02:35 +0530

    Delete 8) Strings directory

--- 8) Strings/_5BuildinFun.cpp
+++
@@ -1,8 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    string str = "Tanmay is a Big fan of MS Dhoni";
-    cout<<str.length()<<endl;
-    cout<<str.size();
-    // yaat \0 add nahi hot size madhe 
-}
