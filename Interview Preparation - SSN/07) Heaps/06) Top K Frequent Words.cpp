613406a8f63c2afed922fbedbab68ecec2251473
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-30 01:22:16 +0530

    Create 06) Top K Frequent Words.cpp

---
+++ Interview Preparation - SSN/07) Heaps/06) Top K Frequent Words.cpp
@@ -0,0 +1,63 @@
+Given an array of strings words and an integer k, return the k most frequent strings.
+
+Return the answer sorted by the frequency from highest to lowest. Sort the words with the same frequency by their lexicographical order.
+
+ 
+
+Example 1:
+
+Input: words = ["i","love","leetcode","i","love","coding"], k = 2
+Output: ["i","love"]
+Explanation: "i" and "love" are the two most frequent words.
+Note that "i" comes before "love" due to a lower alphabetical order.
+Example 2:
+
+Input: words = ["the","day","is","sunny","the","the","the","sunny","is","is"], k = 4
+Output: ["the","is","sunny","day"]
+Explanation: "the", "is", "sunny" and "day" are the four most frequent words, with the number of occurrence being 4, 3, 2 and 1 respectively.
+ 
+
+Constraints:
+
+1 <= words.length <= 500
+1 <= words[i].length <= 10
+words[i] consists of lowercase English letters.
+k is in the range [1, The number of unique words[i]]
+ 
+
+Follow-up: Could you solve it in O(n log(k)) time and O(n) extra space?
+
+
+
+class Solution {
+public:
+    struct cmp {
+        bool operator()(const pair<string, int> &a, const pair<string, int> &b) {
+            if (a.second == b.second)
+                return a.first < b.first;  // reverse lex order for min-heap behavior
+            return a.second > b.second;    // lower freq has higher priority
+        }
+    };
+
+    vector<string> topKFrequent(vector<string>& words, int k) {
+        unordered_map<string, int> freq;
+        for (auto &w : words) freq[w]++;
+
+        priority_queue<pair<string, int>, vector<pair<string, int>>, cmp> pq;
+
+        for (auto &entry : freq) {
+            pq.push({entry.first, entry.second});
+            if (pq.size() > k) pq.pop();
+        }
+
+        vector<string> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top().first);
+            pq.pop();
+        }
+
+        reverse(ans.begin(), ans.end()); // reverse to get highest freq first
+        return ans;
+        
+    }
+};
