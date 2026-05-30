2e8481e17a44de4569f27ac20d738fbba34b57c6
SagarTanmay28 <sagartanmay2004@gmail.com>
2026-05-30 18:57:23 +0530

    Create 90) Longest Happy String.cpp

---
+++ Tanmay's DSA Sheet/90) Longest Happy String.cpp
@@ -0,0 +1,69 @@
+A string s is called happy if it satisfies the following conditions:
+
+s only contains the letters 'a', 'b', and 'c'.
+s does not contain any of "aaa", "bbb", or "ccc" as a substring.
+s contains at most a occurrences of the letter 'a'.
+s contains at most b occurrences of the letter 'b'.
+s contains at most c occurrences of the letter 'c'.
+Given three integers a, b, and c, return the longest possible happy string. If there are multiple longest happy strings, return any of them. If there is no such string, return the empty string "".
+
+A substring is a contiguous sequence of characters within a string.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 1, c = 7
+Output: "ccaccbcc"
+Explanation: "ccbccacc" would also be a correct answer.
+Example 2:
+
+Input: a = 7, b = 1, c = 0
+Output: "aabaa"
+Explanation: It is the only correct answer in this case.
+
+
+class Solution {
+public:
+    typedef pair<int,char> P;
+    string longestDiverseString(int a, int b, int c) {
+        priority_queue<P> pq; // Max Heap;
+
+        // pushing in heap
+        if(a > 0) pq.push({a,'a'});
+        if(b > 0) pq.push({b,'b'});
+        if(c > 0) pq.push({c,'c'});
+
+        string ans = "";
+
+        while(pq.size()){
+            int currCount = pq.top().first;
+            int currChar = pq.top().second;
+            pq.pop();
+
+            // if(consecutive two elements are filled){
+            int n = ans.size();
+            if(n >= 2 && ans[n-1] == currChar && ans[n-2] == currChar){
+                if(pq.size() == 0) return ans; // No elements left other than curr 
+
+                int nextCount = pq.top().first;
+                int nextChar =  pq.top().second;
+                pq.pop();
+
+                ans += nextChar;
+                nextCount--;
+
+                if(nextCount > 0) pq.push({nextCount,nextChar});
+            }
+            else{
+                currCount--;
+                ans += currChar;
+            }
+
+            if(currCount > 0) pq.push({currCount,currChar});
+        }
+
+        return ans;
+        
+    }
+};
