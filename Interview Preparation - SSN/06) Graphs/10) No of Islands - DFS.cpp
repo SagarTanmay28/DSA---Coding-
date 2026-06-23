e935ec12231090d9e61c21bc1fa6cffdb6470624
SagarTanmay28 <247439393+SagarTanmay28@users.noreply.github.com>
2026-06-23 18:38:55 +0530

    Update 10) No of Islands - DFS.cpp

--- Interview Preparation - SSN/06) Graphs/10) No of Islands - DFS.cpp
+++ Interview Preparation - SSN/06) Graphs/10) No of Islands - DFS.cpp
@@ -1,3 +1,54 @@
+// Clean Code :: 
+class Solution {
+public:
+    vector<vector<int>> directions = {{-1,0},{1,0},{0,1},{0,-1}};
+
+    void dfs(int i, int j, vector<vector<char>>& grid, vector<vector<bool>>& vis){
+        int n = grid.size();
+        int m = grid[0].size();
+
+        vis[i][j] = true;
+
+        // 4 directions 
+        for(auto dir : directions){
+            int i_ = i + dir[0];
+            int j_ = j + dir[1];
+
+            if((i_ >= 0 && i_ < n) && (j_ >= 0 && j_ < m)){
+
+                if(!vis[i_][j_] && grid[i_][j_] == '1'){
+                    dfs(i_,j_,grid,vis);
+                }
+            }
+        }
+        
+    }
+
+    int numIslands(vector<vector<char>>& grid) {
+        int n = grid.size();
+        int m = grid[0].size();
+
+        int count = 0;
+
+        vector<vector<bool>> vis(n,vector<bool>(m,false));
+
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+
+                if(!vis[i][j] && grid[i][j] == '1'){
+                    count++;
+                    dfs(i,j,grid,vis);
+                }
+            }
+        }
+
+        return count;
+    }
+};
+
+
+// Solution - 2
+
 int numIslands(vector<vector<char>>& grid) {
         int m = grid.size(), n = grid[0].size();
         int count = 0;
