6c76bbe4f2f52d71c512cdee4cca8cf40eee2190
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-06-28 03:18:13 +0530

    Create 32) Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp

---
+++ Interview Preparation - SSN/06) Graphs/32) Find the City With the Smallest Number of Neighbors at a Threshold Distance.cpp
@@ -0,0 +1,90 @@
+There are n cities numbered from 0 to n-1. Given the array edges where edges[i] = [fromi, toi, weighti] represents a bidirectional and weighted edge between cities fromi and toi, and given the integer distanceThreshold.
+
+Return the city with the smallest number of cities that are reachable through some path and whose distance is at most distanceThreshold, If there are multiple such cities, return the city with the greatest number.
+
+Notice that the distance of a path connecting cities i and j is equal to the sum of the edges' weights along that path.
+
+ 
+
+Example 1:
+
+
+
+Input: n = 4, edges = [[0,1,3],[1,2,1],[1,3,4],[2,3,1]], distanceThreshold = 4
+Output: 3
+Explanation: The figure above describes the graph. 
+The neighboring cities at a distanceThreshold = 4 for each city are:
+City 0 -> [City 1, City 2] 
+City 1 -> [City 0, City 2, City 3] 
+City 2 -> [City 0, City 1, City 3] 
+City 3 -> [City 1, City 2] 
+Cities 0 and 3 have 2 neighboring cities at a distanceThreshold = 4, but we have to return city 3 since it has the greatest number.
+Example 2:
+
+
+
+Input: n = 5, edges = [[0,1,2],[0,4,8],[1,2,3],[1,4,2],[2,3,1],[3,4,1]], distanceThreshold = 2
+Output: 0
+Explanation: The figure above describes the graph. 
+The neighboring cities at a distanceThreshold = 2 for each city are:
+City 0 -> [City 1] 
+City 1 -> [City 0, City 4] 
+City 2 -> [City 3, City 4] 
+City 3 -> [City 2, City 4]
+City 4 -> [City 1, City 2, City 3] 
+The city 0 has 1 neighboring city at a distanceThreshold = 2.
+
+
+
+class Solution {
+public:
+    int findTheCity(int n, vector<vector<int>>& edges, int distanceThreshold) {
+        vector<vector<int>> dist(n, vector<int>(n, 1e9));
+
+        // Distance from a node to itself
+        for (int i = 0; i < n; i++)
+            dist[i][i] = 0;
+
+        // Build graph
+        for (auto &edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+            int wt = edge[2];
+
+            dist[u][v] = wt;
+            dist[v][u] = wt;
+        }
+
+        // Floyd-Warshall
+        for (int via = 0; via < n; via++) {
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    if (dist[i][via] == 1e9 || dist[via][j] == 1e9)
+                        continue;
+
+                    dist[i][j] = min(dist[i][j],
+                                     dist[i][via] + dist[via][j]);
+                }
+            }
+        }
+
+        int city = -1;
+        int minReachable = n;
+
+        for (int i = 0; i < n; i++) {
+            int cnt = 0;
+
+            for (int j = 0; j < n; j++) {
+                if (dist[i][j] <= distanceThreshold)
+                    cnt++;
+            }
+
+            if (cnt <= minReachable) {
+                minReachable = cnt;
+                city = i;
+            }
+        }
+
+        return city;
+    }
+};
