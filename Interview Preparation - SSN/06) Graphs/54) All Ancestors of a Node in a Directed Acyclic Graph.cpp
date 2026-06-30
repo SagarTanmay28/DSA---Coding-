1e9d7667e09f47c245acf6da3942aa26cde2f608
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-07-01 02:04:51 +0530

    Create 54) All Ancestors of a Node in a Directed Acyclic Graph.cpp

---
+++ Interview Preparation - SSN/06) Graphs/54) All Ancestors of a Node in a Directed Acyclic Graph.cpp
@@ -0,0 +1,91 @@
+You are given a positive integer n representing the number of nodes of a Directed Acyclic Graph (DAG). The nodes are numbered from 0 to n - 1 (inclusive).
+
+You are also given a 2D integer array edges, where edges[i] = [fromi, toi] denotes that there is a unidirectional edge from fromi to toi in the graph.
+
+Return a list answer, where answer[i] is the list of ancestors of the ith node, sorted in ascending order.
+
+A node u is an ancestor of another node v if u can reach v via a set of edges.
+
+ 
+
+Example 1:
+
+
+Input: n = 8, edgeList = [[0,3],[0,4],[1,3],[2,4],[2,7],[3,5],[3,6],[3,7],[4,6]]
+Output: [[],[],[],[0,1],[0,2],[0,1,3],[0,1,2,3,4],[0,1,2,3]]
+Explanation:
+The above diagram represents the input graph.
+- Nodes 0, 1, and 2 do not have any ancestors.
+- Node 3 has two ancestors 0 and 1.
+- Node 4 has two ancestors 0 and 2.
+- Node 5 has three ancestors 0, 1, and 3.
+- Node 6 has five ancestors 0, 1, 2, 3, and 4.
+- Node 7 has four ancestors 0, 1, 2, and 3.
+Example 2:
+
+
+Input: n = 5, edgeList = [[0,1],[0,2],[0,3],[0,4],[1,2],[1,3],[1,4],[2,3],[2,4],[3,4]]
+Output: [[],[0],[0,1],[0,1,2],[0,1,2,3]]
+Explanation:
+The above diagram represents the input graph.
+- Node 0 does not have any ancestor.
+- Node 1 has one ancestor 0.
+- Node 2 has two ancestors 0 and 1.
+- Node 3 has three ancestors 0, 1, and 2.
+- Node 4 has four ancestors 0, 1, 2, and 3.
+
+
+class Solution {
+public:
+    vector<vector<int>> getAncestors(int n, vector<vector<int>>& edges) {
+
+        vector<vector<int>> adj(n);
+        vector<int> indegree(n);
+
+        vector<set<int>> ans(n);
+
+        for (auto edge : edges) {
+            int u = edge[0];
+            int v = edge[1];
+
+            adj[u].push_back(v);
+            indegree[v]++;
+        }
+
+        queue<int> q;
+
+        for (int i = 0; i < n; i++) {
+            if (indegree[i] == 0)
+                q.push(i);
+        }
+
+        while (!q.empty()) {
+
+            int u = q.front();
+            q.pop();
+
+            for (int v : adj[u]) {
+
+                // u is a direct ancestor of v
+                ans[v].insert(u);
+
+                // all ancestors of u are also ancestors of v
+                ans[v].insert(ans[u].begin(), ans[u].end());
+
+                indegree[v]--;
+
+                if (indegree[v] == 0)
+                    q.push(v);
+            }
+        }
+
+        vector<vector<int>> final_ans(n);
+
+        for (int i = 0; i < n; i++) {
+            for (int x : ans[i])
+                final_ans[i].push_back(x);
+        }
+
+        return final_ans;
+    }
+};
