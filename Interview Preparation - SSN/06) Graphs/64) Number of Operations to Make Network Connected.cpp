8da17072f4f5a39a5dde3b307f966bb682feac84
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-07-05 01:32:35 +0530

    Create 64) Number of Operations to Make Network Connected.cpp

---
+++ Interview Preparation - SSN/06) Graphs/64) Number of Operations to Make Network Connected.cpp
@@ -0,0 +1,76 @@
+There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
+
+You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
+
+Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
+
+ 
+
+Example 1:
+
+
+Input: n = 4, connections = [[0,1],[0,2],[1,2]]
+Output: 1
+Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
+Example 2:
+
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
+Output: 2
+Example 3:
+
+Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
+Output: -1
+Explanation: There are not enough cables.
+
+class Solution {
+public:
+    
+    vector<int> parent;
+    vector<int> rank;
+
+    int find (int x) {
+        if (x == parent[x]) 
+            return x;
+
+        return parent[x] = find(parent[x]);
+    }
+
+    void Union (int x, int y) {
+        int x_parent = find(x);
+        int y_parent = find(y);
+
+        if (x_parent == y_parent) 
+            return;
+
+        if(rank[x_parent] > rank[y_parent]) {
+            parent[y_parent] = x_parent;
+        } else if(rank[x_parent] < rank[y_parent]) {
+            parent[x_parent] = y_parent;
+        } else {
+            parent[x_parent] = y_parent;
+            rank[y_parent]++;
+        }
+    }
+    
+    int makeConnected(int n, vector<vector<int>>& connections) {
+        
+        if(connections.size() < n-1)
+            return -1;
+        
+        parent.resize(n);
+        rank.resize(n, 0);
+        
+        for(int i = 0; i<n; i++)
+            parent[i] = i;
+        
+        int components = n;
+        for(auto &vec : connections) {
+            if(find(vec[0]) != find(vec[1])) {
+                components--;
+                Union(vec[0], vec[1]);
+            }
+        }
+        return components-1;
+    }
+};
