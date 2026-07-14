daca1caeedf912ebb1688f53a5e35bfba5e5eb4c
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-07-14 19:40:40 +0530

    Create 86) Number of Ways to Arrive at Destination.cpp

---
+++ Interview Preparation - SSN/06) Graphs/86) Number of Ways to Arrive at Destination.cpp
@@ -0,0 +1,91 @@
+You are in a city that consists of n intersections numbered from 0 to n - 1 with bi-directional roads between some intersections. The inputs are generated such that you can reach any intersection from any other intersection and that there is at most one road between any two intersections.
+
+You are given an integer n and a 2D integer array roads where roads[i] = [ui, vi, timei] means that there is a road between intersections ui and vi that takes timei minutes to travel. You want to know in how many ways you can travel from intersection 0 to intersection n - 1 in the shortest amount of time.
+
+Return the number of ways you can arrive at your destination in the shortest amount of time. Since the answer may be large, return it modulo 109 + 7.
+
+ 
+
+Example 1:
+
+
+Input: n = 7, roads = [[0,6,7],[0,1,2],[1,2,3],[1,3,3],[6,3,3],[3,5,1],[6,5,1],[2,5,1],[0,4,5],[4,6,2]]
+Output: 4
+Explanation: The shortest amount of time it takes to go from intersection 0 to intersection 6 is 7 minutes.
+The four ways to get there in 7 minutes are:
+- 0 ➝ 6
+- 0 ➝ 4 ➝ 6
+- 0 ➝ 1 ➝ 2 ➝ 5 ➝ 6
+- 0 ➝ 1 ➝ 3 ➝ 5 ➝ 6
+Example 2:
+
+Input: n = 2, roads = [[1,0,10]]
+Output: 1
+Explanation: There is only one way to go from intersection 0 to intersection 1, and it takes 10 minutes.
+
+
+class Solution {
+public:
+    int countPaths(int n, vector<vector<int>>& roads) {
+
+        const int MOD = 1e9 + 7;
+
+        vector<vector<pair<int,int>>> adj(n);
+
+        for (auto it : roads) {
+
+            int u = it[0];
+            int v = it[1];
+            int wt = it[2];
+
+            adj[u].push_back({v, wt});
+            adj[v].push_back({u, wt});
+        }
+
+        priority_queue<pair<long long,int>,
+                       vector<pair<long long,int>>,
+                       greater<pair<long long,int>>> pq;
+
+        vector<long long> dist(n, LLONG_MAX);
+        vector<int> ways(n, 0);
+
+        dist[0] = 0;
+        ways[0] = 1;
+
+        pq.push({0, 0});
+
+        while (!pq.empty()) {
+
+            auto top = pq.top();
+            pq.pop();
+
+            long long dis = top.first;
+            int node = top.second;
+
+            if (dis > dist[node])
+                continue;
+
+            for (auto it : adj[node]) {
+
+                int adjNode = it.first;
+                int edgeWt = it.second;
+
+                if (dis + edgeWt < dist[adjNode]) {
+
+                    dist[adjNode] = dis + edgeWt;
+                    ways[adjNode] = ways[node];
+
+                    pq.push({dist[adjNode], adjNode});
+                }
+
+                else if (dis + edgeWt == dist[adjNode]) {
+
+                    ways[adjNode] =
+                        (ways[adjNode] + ways[node]) % MOD;
+                }
+            }
+        }
+
+        return ways[n - 1];
+    }
+};
