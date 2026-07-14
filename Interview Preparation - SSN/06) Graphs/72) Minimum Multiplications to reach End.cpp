d54771591265c417b0aaec3e87f661c6e2d19d43
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-07-14 19:32:11 +0530

    Create 72) Minimum Multiplications to reach End.cpp

---
+++ Interview Preparation - SSN/06) Graphs/72) Minimum Multiplications to reach End.cpp
@@ -0,0 +1,64 @@
+Given two integers, start and end, along with an array of integers arr[]. In one operation, you can multiply the current value by any element from arr[], and then take the result modulo 1000 to obtain a new value.
+
+Find the minimum steps in which end can be achieved starting from start. If it is not possible to reach end, then return -1.
+
+Examples :
+
+Input: arr[] = [2, 5, 7], start = 3, end = 30
+Output: 2
+Explanation:
+Step 1: 3*2 = 6 % 1000 = 6 
+Step 2: 6*5 = 30 % 1000 = 30
+Input: arr[] = [3, 4, 65], start = 7, end = 175
+Output: 4
+Explanation:
+Step 1: 7 * 3 = 21 % 1000 = 21  
+Step 2: 21 * 3 = 63 % 1000 = 63  
+Step 3: 63 * 65 = 4095 % 1000 = 95  
+Step 4: 95 * 65 = 6175 % 1000 = 175 
+Input: arr[] = [2, 4], start = 3, end = 5
+Output: -1
+Explanation: Starting from 3 and multiplying by 2 or 4 always produces even numbers after the first step. Since 5 is odd, it can never be reached.
+Constraints:
+1  ≤ arr.size()  ≤ 103
+1  ≤ arr[i]  ≤ 103
+0  ≤ start, end  < 103
+
+
+class Solution {
+public:
+    int minSteps(vector<int>& arr, int start, int end) {
+
+        vector<int> dist(1000, INT_MAX);
+
+        queue<pair<int,int>> q;
+
+        q.push({start, 0});
+        dist[start] = 0;
+
+        while (!q.empty()) {
+
+            auto top = q.front();
+            q.pop();
+
+            int node = top.first;
+            int steps = top.second;
+
+            if (node == end)
+                return steps;
+
+            for (auto num : arr) {
+
+                int newNode = (node * num) % 1000;
+
+                if (steps + 1 < dist[newNode]) {
+
+                    dist[newNode] = steps + 1;
+                    q.push({newNode, steps + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
+};
