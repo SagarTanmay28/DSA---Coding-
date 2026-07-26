41e3501b4787538c86387319102f8e8071a697fa
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-07-26 22:21:35 +0530

    Update 36) Longest Palindromic Substring.cpp

--- Interview Preparation - SSN/01) Dynamic Programming/36) Longest Palindromic Substring.cpp
+++ Interview Preparation - SSN/01) Dynamic Programming/36) Longest Palindromic Substring.cpp
@@ -2,49 +2,41 @@ class Solution {
 public:
     string longestPalindrome(string s) {
         int n = s.size();
-        int maxi = INT_MIN;
-        string ans = "";
 
-        vector<vector<int>> dp(n,vector<int>(n,0));
-
-        // is Palindrome 
-        for(int k=0;k<n;k++){
-            int i = 0, j = k;
-
-            while(j < n){
-                // length = 1
-                if(i == j){
-                    dp[i][j] = 1;
-                    if(j-i+1 > maxi){
-                        maxi = j-i+1;
-                        ans = s.substr(i,j-i+1);
-                    }
-                }
-                // length = 2
-                else if(j-i+1 == 2 && s[i] == s[j]){
-                    dp[i][j] = 1;
-                    if(j-i+1 > maxi){
-                        maxi = j-i+1;
-                        ans = s.substr(i,j-i+1);
-                    }
-                }
-                // length > 2
-                else{
-                    if(s[i] == s[j] && dp[i+1][j-1] == 1){
-                        dp[i][j] = 1;
-                        if(j-i+1 > maxi){
-                            maxi = j-i+1;
-                            ans = s.substr(i,j-i+1);
-                        }
-                    }
-                }
-
-                i++;
-                j++;
-                
+        vector<vector<bool>> dp(n,vector<bool>(n,false));
+        int start_idx = -1;
+        int maxLen    = -1;
+        
+        // len = 1
+        for(int i=0;i<n;i++){
+            dp[i][i] = true;
+            maxLen = 1;
+            start_idx = i;
+        }
+        // len = 2
+        for(int i=0;i<n-1;i++){
+            if(s[i] == s[i+1]){
+                dp[i][i+1] = true;
+                maxLen = 2;
+                start_idx = i;
             }
         }
-
-        return ans;
+        // len >= 3
+        for(int len = 3; len <= n; len++){
+            for(int i=0; i < n - len + 1; i++){
+                int j = i + len - 1; 
+                // j < n
+                // i + len - 1 < n 
+                // i < n - len + 1
+                if(s[i] == s[j] && dp[i+1][j-1] == true){
+                    dp[i][j] = true;
+                    maxLen = len;
+                    start_idx = i;
+                }
+            }
+        }
+        
+        return s.substr(start_idx,maxLen);
+        
     }
 };
