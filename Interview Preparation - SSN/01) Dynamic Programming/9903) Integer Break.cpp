900e9cd5892846f16ca302361c1a71e19f62be63
SAGAR TANMAY SATYAVRATA <247439393+SagarTanmay28@users.noreply.github.com>
2026-08-29 19:46:48 +0530

    Create 9903) Integer Break.cpp

---
+++ Interview Preparation - SSN/01) Dynamic Programming/9903) Integer Break.cpp
@@ -0,0 +1,44 @@
+Given an integer n, break it into the sum of k positive integers, where k >= 2, and maximize the product of those integers.
+
+Return the maximum product you can get.
+
+ 
+
+Example 1:
+
+Input: n = 2
+Output: 1
+Explanation: 2 = 1 + 1, 1 × 1 = 1.
+Example 2:
+
+Input: n = 10
+Output: 36
+Explanation: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36.
+ 
+
+Constraints:
+
+2 <= n <= 58
+
+class Solution {
+public:
+    int helper(int n, vector<int>& dp){
+        if(dp[n] != -1) return dp[n];
+
+        int maxi = 1;
+
+        for(int i=1;i<n;i++){
+            int noSplit = i * (n-i);
+            int split = i * helper(n-i,dp);
+            maxi = max({maxi,noSplit,split});
+        }
+
+        return dp[n] = maxi;
+    }
+    int integerBreak(int n) {
+        vector<int> dp(n+1,-1);
+
+        return helper(n,dp);
+        
+    }
+};
